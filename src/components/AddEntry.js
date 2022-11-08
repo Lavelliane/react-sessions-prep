4e09739453298237a73e173cacea343ccd876a1d
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-11-08 14:44:01 +0800

    Form state

--- src/components/AddEntry.js
+++ src/components/AddEntry.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddEntry() {
+  const [text, setText] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className="add-form">
       <div className="form-control">
         <label htmlFor="task">Task</label>
-        <input id="task" type="text" placeholder="Add Task" />
+        <input
+          id="task"
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="quantity">Quantity</label>
-        <input id="quantity" type="text" placeholder="Add Quantity" />
+        <input
+          id="quantity"
+          type="text"
+          placeholder="Add Quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
       </div>
       <div className="form-control form-control-check">
         <label htmlFor="reminder">Set Reminder</label>
-        <input id="reminder" type="checkbox" />
+        <input
+          id="reminder"
+          type="checkbox"
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
 
       <input type="submit" value="Save Task" className="btn btn-block" />
