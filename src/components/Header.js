6e0bcca503cb941e38647751c9319f836b4b83ef
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-11-10 20:09:28 +0800

    Add button conditional rendering and links refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 export default function Header({ title, showAddItem, setShowAddItem }) {
+  const location = useLocation();
+
   function onClick() {
     setShowAddItem(!showAddItem);
   }
@@ -8,11 +11,13 @@ export default function Header({ title, showAddItem, setShowAddItem }) {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAddItem ? "red" : "green"}
-        text={showAddItem ? "Close" : "Add"}
-        onClick={onClick}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAddItem ? "red" : "green"}
+          text={showAddItem ? "Close" : "Add"}
+          onClick={onClick}
+        />
+      )}
     </header>
   );
 }
